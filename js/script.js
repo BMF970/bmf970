8d12108c6474cf5015a1a9632dbe15f533a36bae
BMF970 <brianmartines119@yahoo.com>
2022-08-04 21:28:47 -0500

    creando el menu mobile, solo falta hacerlo funcionar con js y el banner

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const botonMenu = document.querySelector(`#card-menu-mobile-img`);
+botonMenu.addEventListener(`click`, displayMenu);
+
+function displayMenuMobile(){
+        var display;
+        var start, end;
+        var display = menu.style.display;
+
+}
